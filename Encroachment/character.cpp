638f8abf30f134ceb9d3380c5416e1779b4be566
julian salazar <julian.salazard@udea.edu.co>
2020-12-23 14:28:48 -0500

    Restricciones de movimiento para la clase Character

--- Encroachment/character.cpp
+++ Encroachment/character.cpp
@@ -7,16 +7,16 @@ Character::Character()
 
 void Character::keyPressEvent(QKeyEvent *event)
 {
-    if(event->key() == Qt::Key_W){
+    if(event->key() == Qt::Key_W and y()+100>360){
         setPos(x(),y()-5);
     }
-    else if(event->key() == Qt::Key_S){
+    else if(event->key() == Qt::Key_S and y()+200<720){
         setPos(x(),y()+5);
     }
-    else if(event->key() == Qt::Key_A){
+    else if(event->key() == Qt::Key_A and x()>190){
         setPos(x()-5,y());
     }
-    else if(event->key() == Qt::Key_D){
+    else if(event->key() == Qt::Key_D and x()+100<1090){
         setPos(x()+5,y());
     }
 }
