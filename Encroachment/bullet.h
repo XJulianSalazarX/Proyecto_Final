075ceb9fafc98e01482cb0534b4538134b7eac27
julian salazar <julian.salazard@udea.edu.co>
2020-12-23 22:35:16 -0500

    Documentación de las clases Bullet y Character

--- Encroachment/bullet.h
+++ Encroachment/bullet.h
@@ -1,3 +1,10 @@
+/**
+  @file bullet.h
+  @version 1.0
+  @date 23/12/2020
+  @title Class Bullet
+  @brief CLase que representa una bala
+  */
 #ifndef BULLET_H
 #define BULLET_H
 
@@ -10,10 +17,21 @@ class Bullet: public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Constructor por defecto de la clase Bullet
+     */
     Bullet();
+    /**
+      * @brief Destructor de la clase Bullet
+      */
     ~Bullet();
 
 public slots:
+    /**
+     * @brief move Mover la clase Bullet 5 pixeles hacia arriba
+     * atraves de la instancia de la clase QGraphicsScene *Scene,
+     * si la clase Bullet sale de la escena es eliminada
+     */
     void move();
 
 private:
--- Encroachment/character.h
+++ Encroachment/character.h
@@ -1,3 +1,11 @@
+/**
+  @file Character.h
+  @version 1.0
+  @date 23/12/2020
+  @title Class Character
+  @brief CLase que representa un carro que se mueve hacia arriba o abajo de un QGraphicsScene
+  dependiendo de las teclas ingresadas
+  */
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
@@ -11,7 +19,15 @@ class Character:public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Constructor de la clase Character
+     */
     Character();
+    /**
+     * @brief keyPressEvent Mover la clase Character o instanciar la clase Bullet
+     * dependiendo de la tecla ingresada
+     * @param event Recibe la tecla ingresada por teclado
+     */
     void keyPressEvent(QKeyEvent *event);
 };
 
